8e1dbb995f4a925076b31efc03fcf8c772d2119c
danil <minakonn9shkoni@gmail.com>
2022-06-24 16:38:38 +0300

    Create sql.session

--- src/models/user.js
+++ src/models/user.js
@@ -17,47 +17,47 @@ module.exports = (sequelize, DataTypes) => {
       firstname: {
         type: DataTypes.STRING(32),
         allowNull: false,
-        validate:{
-          len:[3,32],
+        validate: {
+          len: [3, 32],
         },
       },
-      lastname: { 
-        type: DataTypes.STRING(32), 
+      lastname: {
+        type: DataTypes.STRING(32),
         allowNull: false,
-        validate:{
-          len:[3,32],
+        validate: {
+          len: [3, 32],
         },
       },
-      email: { 
-        type: DataTypes.STRING(32), 
+      email: {
+        type: DataTypes.STRING(32),
         allowNull: false,
-        unique:true,
-        validate:{
-          len:[3,32],
-        }
+        unique: true,
+        validate: {
+          len: [3, 32],
+        },
       },
-      ismale: { 
-        type: DataTypes.BOOLEAN, 
+      ismale: {
+        type: DataTypes.BOOLEAN,
         allowNull: false,
       },
-      dob: { 
-        type: DataTypes.DATEONLY, 
+      dob: {
+        type: DataTypes.DATEONLY,
         allowNull: false,
-        validate:{
-          isAfter:"1900-01-01",
-          isBeforeCurrentDate(value){
+        validate: {
+          isAfter: "1900-01-01",
+          isBeforeCurrentDate(value) {
             const date = new Date(value);
 
-            if(date>new Date()){
-              throw new Error('Cannot set dob after current date');
+            if (date > new Date()) {
+              throw new Error("Cannot set dob after current date");
             }
-          }
-        }
+          },
+        },
       },
-      height: { 
-        type: DataTypes.NUMERIC(3,2), 
-        allowNull: false, 
-        defaultValue:1.5,
+      height: {
+        type: DataTypes.NUMERIC(3, 2),
+        allowNull: false,
+        defaultValue: 1.5,
       },
     },
     {
