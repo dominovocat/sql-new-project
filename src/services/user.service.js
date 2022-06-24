0dbced5cf2e4de026da7f134e321cab8d2a19dac
danil <minakonn9shkoni@gmail.com>
2022-06-24 16:14:25 +0300

    Add simple thing

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,44 +1,43 @@
 const User = require("../models/user");
 
-module.exports.createUser = async (data) => {
-  const newUser = await User.create(data);
+class UserService {
+  createUser = async (data) => {
+    const newUser = await User.create(data);
 
-  return newUser;
+    return newUser;
+  };
+
+  getUserList = async (limit, page) => {
+    const foundUsers = await User.findAll({
+      limit: limit,
+      offset: (page - 1) * limit,
+    });
+
+    return foundUsers;
+  };
+  getUserById = async (userId) => {
+    const user = await User.findByPk(userId);
+    return user;
+  };
+
+  updateUserById = async (id, data) => {
+    await User.update(data, {
+      where: {
+        id: id,
+      },
+    });
+    const updateUser = await User.findByPk(id);
+    return updateUser;
+  };
+
+  deleteUserById = async (id) => {
+    const deleteUser = await User.findByPk(id);
+    if (!deletedUser) {
+      throw new Error("User not found");
+    }
+    await deleteUser.destroy();
+    return deleteUser;
+  };
 }
 
-module.exports.getUserList = async (limit, page) => {
-  const foundUsers = await User.findAll({
-    limit: limit,
-    offset: (page - 1) * limit,
-  });
-
-  return foundUsers;
-}
-module.exports.getUserById = async (userId) => {
-
-
-  const user = await User.findByPk(userId);
-  return user;
-}
-
-module.exports.updateUserById = async (id,data)=>{
-  await User.update(data,{
-    where:{
-      id:id,
-    },
-  });
-  const updateUser = await User.findByPk(id);
-  return updateUser;
-};
-
-module.exports.deleteUserById = async (id)=>{
-
-
-  const deleteUser = await User.findByPk(id);
-  if(!deletedUser){
-    throw new Error('User not found');
-  }
-  await deleteUser.destroy();
-  return deleteUser;
-
-};
+module.exports = UserService();
