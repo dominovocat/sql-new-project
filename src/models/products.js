96d6ed32f5c33450d87b42959cbc2f71150907da
danil <minakonn9shkoni@gmail.com>
2022-06-27 14:48:05 +0300

    Add products table

---
+++ src/models/products.js
@@ -0,0 +1,38 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class products extends Model {
+
+    static associate(models) {
+      // define association here
+    }
+  }
+  products.init({
+    name: {
+      type:DataTypes.STRING(32),
+      allowNull:false,
+    },
+    brand: {
+      type:DataTypes.STRING(32),
+      allowNull:false,
+    },
+    categories: {
+      type:DataTypes.STRING(32),
+      allowNull:false,
+    },
+    price: {
+      type:DataTypes.DECIMAL(10,2),
+      allowNull:false,
+    },
+    amount: {
+      type:DataTypes.DECIMAL(10,2),
+      allowNull:false,
+    },
+  }, {
+    sequelize,
+    modelName: 'products',
+  });
+  return products;
+};
