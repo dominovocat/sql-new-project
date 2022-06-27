e141a72c05d0321a8f7ec14e28acae92aedd67c0
danil <minakonn9shkoni@gmail.com>
2022-06-27 15:26:13 +0300

    Add associate

--- src/models/order.js
+++ src/models/order.js
@@ -1,38 +1,50 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class Order extends Model {
-
     static associate(models) {
       // define association here
+      Order.belongsTo(models.User);
     }
   }
-  Order.init({
-    address: {
-      type:DataTypes.STRING,
-      allowNull:false,
+  Order.init(
+    {
+      address: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      user_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+
+        references: {
+          key: "id",
+          model: "users",
+        },
+      },
+      phones: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          isNumeric: true,
+        },
+      },
+      status: {
+        type: Sequelize.ENUM(
+          "prepared",
+          "paid",
+          "shipping",
+          "shiped",
+          "received"
+        ),
+        allowNull: false,
+      },
     },
-    user_id:{
-      type:DataTypes.INTEGER,
-      allowNull:false,
-    },
-    phones: {
-      type:DataTypes.STRING,
-      allowNull:false,
-      validate:{
-        isNumeric:true,
-      }
-    },
-    status: {
-      type: Sequelize.ENUM('prepared','paid','shipping','shiped','received'),
-      allowNull:false,
-    },
-  }, {
-    sequelize,
-    modelName: 'Order',
-    tableName:'orders',
-  });
+    {
+      sequelize,
+      modelName: "Order",
+      tableName: "orders",
+    }
+  );
   return Order;
 };
