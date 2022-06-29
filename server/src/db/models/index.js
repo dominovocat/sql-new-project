00045c07f2812ef5c410890fa73c3cecf0ca234c
danil <minakonn9shkoni@gmail.com>
2022-06-29 15:02:52 +0300

    add server & client folders

--- server/src/db/models/index.js
+++ server/src/db/models/index.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
 const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
+const config = require(__dirname + '../../config/database.js')[env];
 const db = {};
 
 let sequelize;
